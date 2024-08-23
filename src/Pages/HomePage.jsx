13971bbe4031b8b44b999341dab96322765a6519
Truong <truongcao3320@gmail.com>
2024-08-23 10:10:42 +0700

    Add more comments in home page

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -16,6 +16,7 @@ const HomePage = () => {
   return (
     <>
       {/* <Navbar /> */}
+      {/* Add more comments to test git command in Home Page */}
       <Home />
       <Middle />
       <Destinations />
