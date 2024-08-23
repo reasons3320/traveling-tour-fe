e9b9bf820466b754c3cbe72dc96c02e9b91f82b5
Truong <truongcao3320@gmail.com>
2024-08-23 14:54:34 +0700

    2:54pm

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -18,6 +18,7 @@ const HomePage = () => {
       {/* <Navbar /> */}
       {/* Add more comments to test git command in Home Page */}
       {/* Test again at 2:51 pm */}
+      {/* Add more test at 2:53 pm */}
       <Home />
       <Middle />
       <Destinations />
